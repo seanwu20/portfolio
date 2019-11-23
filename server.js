ebac91857cf0a4fb0aa64b5de119ecb7c29b3824
Sean Wu <seanwu20@gmail.com>
2019-11-22 22:25:34 -0500

    react app deployed

--- server.js
+++ server.js
@@ -4,12 +4,8 @@ const express = require('express');
 const path = require('path');
 
 const server = express();
-console.log(process.cwd());
 server.use(express.static(path.join(process.cwd() + '/build')));
 
-server.get('*.*', express.static(path.join(process.cwd() + '/build'), {
-    maxAge: '1y'
-}));
 
 server.get('*', (req, res) => {
     res.sendFile(path.join(process.cwd() + '/build/index.html'));
