9751ffdca09b9c9d45b047681eec75968f6f28be
Sean Wu <seanwu20@gmail.com>
2020-02-12 22:50:26 -0500

    attempting cicd

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -7,7 +7,6 @@ export const About = () => {
     return (
         <div className='about'>
             <p>About this site</p>
-        testing
             <p>> Deployed with S3 Buckets </p>
             <p>> CI/CD with AWS CodePipeline</p>
             <p>> CDN setup with Cloudfront </p>
